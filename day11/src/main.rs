15a91f3447c605a8c4a4028223984d5ecf948dec
ehorrent <emmanuel@horrent.fr>
2025-12-11 20:41:02 +0100

    feat(day11): day 11 resolution

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,50 +1,103 @@
-struct Device {
-    name: String,
-    targets: Vec<String>,
+use std::collections::HashMap;
+
+struct Device<'a> {
+    name: &'a str,
+    targets: Vec<&'a str>,
 }
 
-fn parse_input(raw_data: &str) -> Vec<Device> {
-    raw_data
+fn parse_input(raw_data: &str) -> Vec<Device<'_>> {
+    let mut devices: Vec<Device> = raw_data
         .lines()
         .map(|line| {
-            let mut name_target_split = line.split(": ");
-
-            let source_device = name_target_split.nth(0).unwrap();
-            let target_devices = name_target_split
-                .nth(0)
-                .unwrap()
-                .split(' ')
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>();
+            let name_target_split: Vec<&str> = line.split(": ").collect();
+            let source_device = name_target_split[0];
+            let target_devices = name_target_split[1].split(' ').collect::<Vec<&str>>();
 
             Device {
-                name: source_device.to_string(),
+                name: source_device,
                 targets: target_devices,
             }
         })
-        .collect()
+        .collect();
+
+    devices.push(Device {
+        name: "out",
+        targets: vec![],
+    });
+
+    devices
 }
 
-fn solve_part1() -> usize {
-    0
+fn count_paths_rec(
+    device_name: &str,
+    target_device_name: &str,
+    devices: &Vec<Device>,
+    count_cache: &mut HashMap<String, usize>,
+) -> usize {
+    // Found a path!
+    if device_name == target_device_name {
+        return 1;
+    }
+
+    // Already processed
+    if let Some(&count) = count_cache.get(device_name) {
+        return count;
+    }
+
+    // Recursion over next devices
+    let device = devices.iter().find(|d| d.name == device_name).unwrap();
+    let mut count = 0;
+    for next_device_name in &device.targets {
+        count += count_paths_rec(next_device_name, target_device_name, devices, count_cache);
+    }
+
+    count_cache.insert(device_name.to_string(), count);
+    count
 }
 
-fn solve_part2() -> usize {
-    0
+fn count_paths(start_device_name: &str, path: &[&str], devices: &Vec<Device>) -> usize {
+    let mut current_device_name = start_device_name;
+
+    let mut total = 1;
+    for target_device_name in path {
+        let paths_count = count_paths_rec(
+            current_device_name,
+            target_device_name,
+            devices,
+            &mut HashMap::new(),
+        );
+        if paths_count == 0 {
+            return 0;
+        }
+
+        total *= paths_count;
+        current_device_name = target_device_name;
+    }
+
+    total
+}
+
+fn solve_part1(devices: &Vec<Device>) -> usize {
+    count_paths_rec("you", "out", devices, &mut HashMap::new())
+}
+
+fn solve_part2(devices: &Vec<Device>) -> usize {
+    count_paths("svr", &["dac", "fft", "out"], devices)
+        + count_paths("svr", &["fft", "dac", "out"], devices)
 }
 
 fn main() {
     let raw_data = include_str!("../input/input-11.txt");
     let input = parse_input(raw_data);
 
-    println!("Solver - day 10:");
+    println!("Solver - day 11:");
 
     // Part 1
-    let result = solve_part1();
+    let result = solve_part1(&input);
     println!("  Part 1 - Final code: {}", result);
 
     // Part 2
-    let result = solve_part2();
+    let result = solve_part2(&input);
     println!("  Part 2 - Final code: {}", result);
 }
 
@@ -52,7 +105,9 @@ fn main() {
 mod tests {
     use super::*;
 
-    const RAW_INPUT: &str = "aaa: you hhh
+    #[test]
+    fn check_part1_result() {
+        let raw_data = "aaa: you hhh
 you: bbb ccc
 bbb: ddd eee
 ccc: ddd eee fff
@@ -63,17 +118,29 @@ ggg: out
 hhh: ccc fff iii
 iii: out";
 
-    #[test]
-    fn check_part1_result() {
-        let input = parse_input(RAW_INPUT);
-        let result = solve_part1();
+        let input = parse_input(raw_data);
+        let result = solve_part1(&input);
         assert_eq!(result, 5);
     }
 
-    // #[test]
-    // fn check_part2_result() {
-    //     let input = parse_input(RAW_INPUT);
-    //     let result = solve_part2();
-    //     assert_eq!(result, 6);
-    // }
+    #[test]
+    fn check_part2_result() {
+        let raw_data = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
+
+        let input = parse_input(raw_data);
+        let result = solve_part2(&input);
+        assert_eq!(result, 2);
+    }
 }
