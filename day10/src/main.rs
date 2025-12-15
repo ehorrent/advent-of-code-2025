ba9c72ce4c140bf4a38fdf3d1b63de9cfc60903b
ehorrent <emmanuel@horrent.fr>
2025-12-15 21:50:57 +0100

    feat(day10): finally day 10 resolution, but used a solver...

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,25 +1,30 @@
+use microlp::{ComparisonOp, LinearExpr, OptimizationDirection, Problem};
 use regex::Regex;
 use std::collections::HashMap;
 
-struct Machine {
-    expected_lights: usize,
-    buttons: Vec<usize>,
-    _joltages: Vec<usize>,
+struct JoltageMachine {
+    expected_joltage: Vec<usize>,
+    buttons: Vec<Vec<usize>>,
 }
 
-struct Context<'a> {
+struct LightMachine {
+    expected_lights: usize,
+    buttons: Vec<usize>,
+}
+
+struct LightContext<'a> {
     light_state: usize,
     step: usize,
     best_step_count: &'a mut Option<usize>,
     state_cost: &'a mut HashMap<usize, usize>,
 }
 
-impl Machine {
+impl LightMachine {
     fn toggle(&self, button_mask: usize, light_state: usize) -> usize {
         light_state ^ button_mask
     }
 
-    fn solve_rec(&self, context: Context) {
+    fn solve_rec(&self, context: LightContext) {
         // Check if we already reached this state with less steps
         if let Some(cost) = context.state_cost.get(&context.light_state)
             && *cost < context.step
@@ -50,7 +55,7 @@ impl Machine {
         for button in self.buttons.iter() {
             let next_state = self.toggle(*button, context.light_state);
 
-            let next_context = Context {
+            let next_context = LightContext {
                 light_state: next_state,
                 step: context.step + 1,
                 best_step_count: context.best_step_count,
@@ -64,7 +69,7 @@ impl Machine {
     fn solve(&self) -> usize {
         let mut best_step_count = None;
         let mut state_cost = HashMap::new();
-        self.solve_rec(Context {
+        self.solve_rec(LightContext {
             light_state: 0,
             step: 0,
             best_step_count: &mut best_step_count,
@@ -79,7 +84,8 @@ impl Machine {
     }
 }
 
-fn parse_input(raw_data: &str) -> Vec<Machine> {
+fn parse_input(raw_data: &str) -> (Vec<LightMachine>, Vec<JoltageMachine>) {
+    // I'm lazy...
     let lights_re = Regex::new(r"\[(.*?)\]").unwrap();
     let buttons_re = Regex::new(r"\((.*?)\)").unwrap();
     let joltages_re = Regex::new(r"\{(.*?)\}").unwrap();
@@ -98,16 +104,13 @@ fn parse_input(raw_data: &str) -> Vec<Machine> {
                 })
                 .sum();
 
-            let buttons: Vec<usize> = buttons_re
+            let buttons: Vec<Vec<usize>> = buttons_re
                 .captures_iter(line)
                 .map(|cap| {
                     cap[1]
                         .split(',')
-                        .map(|s| {
-                            let index: usize = s.trim().parse().unwrap();
-                            1 << index
-                        })
-                        .sum()
+                        .map(|s| s.trim().parse().unwrap())
+                        .collect::<Vec<usize>>()
                 })
                 .collect();
 
@@ -121,16 +124,24 @@ fn parse_input(raw_data: &str) -> Vec<Machine> {
                 })
                 .collect();
 
-            Machine {
-                expected_lights,
-                buttons,
-                _joltages: joltages,
-            }
+            (
+                LightMachine {
+                    expected_lights,
+                    buttons: buttons
+                        .iter()
+                        .map(|button| button.iter().map(|index| 1 << *index).sum())
+                        .collect(),
+                },
+                JoltageMachine {
+                    expected_joltage: joltages,
+                    buttons,
+                },
+            )
         })
         .collect()
 }
 
-fn solve_part1(machines: &Vec<Machine>) -> usize {
+fn solve_part1(machines: &Vec<LightMachine>) -> usize {
     let mut total = 0;
     for machine in machines.iter() {
         total += machine.solve();
@@ -139,22 +150,64 @@ fn solve_part1(machines: &Vec<Machine>) -> usize {
     total
 }
 
-fn solve_part2() -> i32 {
-    0
+fn solve_part2(machines: &Vec<JoltageMachine>) -> usize {
+    let mut total = 0;
+
+    for machine in machines.iter() {
+        let mut problem = Problem::new(OptimizationDirection::Minimize);
+
+        // Add variables representing button presses
+        let mut press_count_vars = Vec::new();
+        for button in &machine.buttons {
+            press_count_vars.push(
+                problem.add_integer_var(
+                    1.0,
+                    (
+                        0,
+                        // Get the max possible number of presses for this button
+                        button
+                            .iter()
+                            .map(|index| machine.expected_joltage[*index] as i32)
+                            .min()
+                            .unwrap(),
+                    ),
+                ),
+            );
+        }
+
+        // Create expression for each expected joltage
+        for (joltage_index, expected_joltage) in machine.expected_joltage.iter().enumerate() {
+            let mut expression = LinearExpr::empty();
+            for (button_index, button) in machine.buttons.iter().enumerate() {
+                if button.contains(&joltage_index) {
+                    let press_count_var = press_count_vars[button_index];
+                    expression.add(press_count_var, 1_f64);
+                }
+            }
+
+            problem.add_constraint(expression, ComparisonOp::Eq, *expected_joltage as f64);
+        }
+
+        // Solve the "press count" problem
+        let solution = problem.solve().unwrap();
+        total += solution.objective().round() as usize;
+    }
+
+    total
 }
 
 fn main() {
     let raw_data = include_str!("../input/input-10.txt");
-    let input = parse_input(raw_data);
+    let (light_machines, joltage_machines) = parse_input(raw_data);
 
     println!("Solver - day 10:");
 
     // Part 1
-    let result = solve_part1(&input);
+    let result = solve_part1(&light_machines);
     println!("  Part 1 - Final code: {}", result);
 
     // Part 2
-    let result = solve_part2();
+    let result = solve_part2(&joltage_machines);
     println!("  Part 2 - Final code: {}", result);
 }
 
@@ -168,15 +221,15 @@ mod tests {
 
     #[test]
     fn check_part1_result() {
-        let input = parse_input(RAW_INPUT);
-        let result = solve_part1(&input);
+        let (light_machines, _) = parse_input(RAW_INPUT);
+        let result = solve_part1(&light_machines);
         assert_eq!(result, 7);
     }
 
-    // #[test]
-    // fn check_part2_result() {
-    //     let input = parse_input(RAW_INPUT);
-    //     let result = solve_part2();
-    //     assert_eq!(result, 6);
-    // }
+    #[test]
+    fn check_part2_result() {
+        let (_, joltage_machines) = parse_input(RAW_INPUT);
+        let result = solve_part2(&joltage_machines);
+        assert_eq!(result, 33);
+    }
 }
